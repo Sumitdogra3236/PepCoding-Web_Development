895df04f40bdb6b7767a38f84b9907ff50eb97aa
SUMIT <sumitdogra3236@gmail.com>
2021-10-06 10:54:51 +0530

    scap cricinfo

---
+++ Scrap_CricInfo/highestWicketTaker.js
@@ -0,0 +1,39 @@
+// To find the Highest Wicket Taker
+const request = require("request");
+const cheerio = require("cheerio");
+const chalk = require("chalk");
+
+request("https://www.espncricinfo.com/series/ipl-2020-21-1210595/chennai-super-kings-vs-kings-xi-punjab-53rd-match-1216506/full-scorecard", cb);
+
+function cb(err, res, html){
+    if(err){
+        console.log(err);
+    }
+    else {
+        hwt(html);
+    }
+}
+
+function hwt(html){
+    let $ = cheerio.load(html);
+    let teamArr = $(".match-info.match-info-MATCH .team");
+    
+    for(let i = 0; i < teamArr.length; i++){
+        let hasclass = $(teamArr[i]).hasClass("team-gray");
+        if(hasclass == false){
+            let wteamName = $(teamArr[i]).find(".name");
+            // console.log(wteamName.text())
+        }
+    }
+
+    let inningArr = $(".card.content-block.match-scorecard-table .Collapsible");
+    let inningHtml =  inningArr.html();
+    
+    for(let i = 0; i < inningArr.length; i++){
+        let inningHtml = $(inningArr[i]).html();
+        let teamName = $(".header-title.label");
+        console.log(teamName.text())
+    }
+
+
+}
